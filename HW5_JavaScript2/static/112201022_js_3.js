d63861b6c699ae2a12af5fb5b44597279895ce88
KOYUNGCHING <cordeliake@gmail.com>
2025-10-02 14:18:16 +0800

    js3 猜數字時提示字改成寫畫面上不用alert

--- HW5_JavaScript2/static/112201022_js_3.js
+++ HW5_JavaScript2/static/112201022_js_3.js
@@ -10,12 +10,19 @@ function checkGauss() {
     if (input === "") return alert("請先輸入數字！");
     let g = +input.value;
     if (!Number.isInteger(g) || g < 0 || g > 100) return alert("請輸入 0 到 100 的整數。");
-    if (!startTime) startTime = Date.now();
-    attempts++;
 
-    if (g > answer) alert("太大了！再試一次～ 數字介於 0 ~ " + g);
-    else if (g < answer) alert("太小了！再試一次～ 數字介於 " + g + " ~ 100");
-    else {
+    if (!startTime) {
+        startTime = Date.now();
+        timerId = setInterval(() => {
+            const t = (Date.now() - startTime) / 1000;
+            tim.textContent = `用時：${t.toFixed(2)} 秒`;
+        }, 100);
+    }
+    attempts++;
+    if (g > answer) { msg.textContent = `太大了！再試一次～`; return; }
+    if (g < answer) { msg.textContent = `太小了！再試一次～`; return; }
+
+    {
         let t = ((Date.now() - startTime) / 1000).toFixed(2);
         msg.textContent = `猜中了！共猜了 ${attempts} 次，花了 ${t} 秒`;
         his.innerHTML += `<div>猜了 ${attempts} 次，耗時 ${t} 秒， ${new Date().toLocaleTimeString()}</div>`;
